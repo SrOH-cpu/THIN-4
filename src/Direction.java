57ce55c9a1f9acb503de5ad7ed5eeb28231aa93b
SrOH-cpu <dswehrli@gmx.ch>
2026-05-08 03:21:52 +0200

    fixes

--- src/Direction.java
+++ src/Direction.java
@@ -3,12 +3,13 @@ public enum Direction{
     RIGHT;
 
     public static Direction getDirection(String s){
+        s = s.trim();
         if("00".equals(s)){
             return RIGHT;
         }else if("0".equals(s)){
             return LEFT;
         }else{
-            throw new IllegalArgumentException("Invalid Direction");
+            throw new IllegalArgumentException("Invalid Direction: " + s);
         }
     }
 }
